141ddb225f0a1c17f6130ab9a8f12d2f64692d71
Sebastien Luong <sebastienluong@gmail.com>
2016-11-02 21:43:45 -0700

    Draw circle within grid

--- ticTacToe/app.js
+++ ticTacToe/app.js
@@ -6,13 +6,45 @@ function drawBoard() {
     const canvas = document.getElementById('canvas');
     const context = document.getElementById('canvas').getContext('2d');
 
+    let maxBounds = [];
     for (let i=0; i<3; i++ ){
         let spaceY = i+1;
+        let maxBound = i+2;
+        maxBounds[i] = 50*(maxBound);
 
         for (let j=0; j<3; j++){
             let spaceX = j+1;
 
             context.strokeRect(50*spaceX, 50*spaceY, 50, 50);
         }
+    }
+
+    let onmousedown = function(e){
+        let mouseX = e.pageX - this.offsetLeft;
+        let mouseY = e.pageY - this.offsetTop;
+        let x = isGreaterThan(maxBounds, mouseX);
+        let y = isGreaterThan(maxBounds, mouseY);
+        console.log("X coord: "+mouseX);
+        console.log("Y coord: "+mouseY);
+        console.log("X quadrant: "+x);
+        console.log("Y coord: "+y);
+        drawCircle(x, y);
+    };
+
+    canvas.addEventListener("mousedown", onmousedown);
+
+    function isGreaterThan(maxArray, pos) {
+        for (let i=0; i<maxArray.length; i++){
+            if (maxArray[i]>pos){
+                return maxArray[i];
+            }
+        }
+        return 0;
+    }
+
+    function drawCircle(x, y) {
+        context.beginPath();
+        context.arc(x-25, y-25, 20, 0, 2*Math.PI);
+        context.stroke();
     }
 }
